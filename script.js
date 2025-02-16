964a416e2ab65b15cf34b1c4ec6ff39de9966919
Solariis <florian.gagnier@epita.fr>
2025-02-16 16:14:29 +0100

    pas mal d'update de style + contact

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 let currentIndex = 1;
-const totalItems = 2; // Update this to the new total number of items
-const intervalTime = 10000; // 10 seconds
+const totalItems = 2;
+const intervalTime = 10000;
 
 setInterval(() => {
     document.getElementById(`carousel-${currentIndex}`).checked = false;
     currentIndex = currentIndex % totalItems + 1;
     document.getElementById(`carousel-${currentIndex}`).checked = true;
 }, intervalTime);
+
+
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const successMessage = document.getElementById('successMessage');
+    successMessage.style.display = 'block';
+
+    setTimeout(() => {
+        successMessage.style.display = 'none';
+    }, 2000);
+
+    this.reset();
+});
